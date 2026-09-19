ae886ab6ed0304cad6e321e942ce1dda2211598f
pixa-social <56717855+pixa-social@users.noreply.github.com>
2026-09-19 23:19:23 +0530

    fix: api/outreach.js reads bundled api/d0-d3.txt shards

--- api/outreach.js
+++ api/outreach.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const path = require('path');
 module.exports = (req, res) => {
   try {
-    const B64 = fs.readFileSync(path.join(__dirname, 'outreach-data.b64'), 'utf8');
+    const B64 = [0, 1, 2, 3].map((i) =>
+      fs.readFileSync(path.join(__dirname, 'd' + i + '.txt'), 'utf8')
+    ).join('');
     const buf = zlib.gunzipSync(Buffer.from(B64, 'base64'));
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.setHeader('Cache-Control', 'public, max-age=60');
